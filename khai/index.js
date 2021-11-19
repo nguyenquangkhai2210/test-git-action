53e30105ba9b584bc3db84bde60bf3e410c3ddef
Nguyễn Quang Khải <nguyenquangkhai2210@gmail.com>
2021-11-19 09:39:31 +0700

    test 2

--- khai/index.js
+++ khai/index.js
@@ -1,7 +1,13 @@
 const core = require('@actions/core');
 
+const test = () => {
+  
 try {
   console.log(`Good by!`);
 } catch (error) { 
   core.setFailed(error.message);
 }
+process.exit(0);
+}
+
+test();
