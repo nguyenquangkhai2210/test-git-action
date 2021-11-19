95f6b2879942485ff7713651d8ae9a66016aac08
Nguyễn Quang Khải <nguyenquangkhai2210@gmail.com>
2021-11-19 09:31:25 +0700

    test 2

--- khai/index.js
+++ khai/index.js
@@ -1,15 +1,7 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Good by ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`Good by!`);
 } catch (error) { 
   core.setFailed(error.message);
 }
